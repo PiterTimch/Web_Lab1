33709081ac13b71a8e49e846b14d83ef21c78ad9
Petro <petrotymchuck@gmail.com>
2026-02-18 11:16:13 +0200

    some fixes

--- public/index.php
+++ public/index.php
@@ -6,12 +6,13 @@ declare(strict_types=1);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Лабораторна робота №1 (варіант 11)</title>
+    <title>Лабораторна робота №1</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
     <h1>Лабораторна робота №1</h1>
+    <h2>Тимчук Петро: ІПЗ-22, варіант 11</h2>
 
     <div class="button-container">
         <?php for ($i = 1; $i <= 6; $i++): ?>
